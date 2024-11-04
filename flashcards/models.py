ec45bef358f5358d1537919fd7a0b3acd89ef6d2
WeebBot#2 <124053467+MiaNSlater@users.noreply.github.com>
2024-11-04 11:18:33 +0000

    Edited models.py, added Tailwind CSS framework

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
+class DifficultyLevel(models.TextChoices):
+        EASY = 'E', 'Easy'
+        MEDIUM = 'M', 'Medium'
+        HARD = 'H', 'Hard'
+
 class Flashcard(models.Model):
     question = models.CharField(max_length=200)
     answer = models.CharField(max_length=200)
-    #difficulty = models.enums()
+    difficulty = models.CharField(
+        max_length=1,
+        choices=DifficultyLevel.choices,
+        default=None,
+        null=True,
+        blank=True,
+    )
+    
+class FlashcardSet(models.Model):
+      name = models.CharField(max_length=200)
+      cards = models.ManyToManyField(Flashcard, related_name="sets")
+      created_at = models.DateTimeField()
