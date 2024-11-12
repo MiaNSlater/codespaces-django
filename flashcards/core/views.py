e5029b7881909c7c247c98c3206cdb3e0fcaa42e
WeebBot#2 <124053467+MiaNSlater@users.noreply.github.com>
2024-11-12 14:53:42 +0000

    Set up user list function. Added path to user list in urls.py

--- flashcards/core/views.py
+++ flashcards/core/views.py
@@ -1,19 +1,26 @@
+#from django.http import JsonResponse
 import json
-
 from django.shortcuts import render
-from django.core.serializers import serialize
 from flashcards.models import User
 from flashcards.models import Flashcard
 from flashcards.models import FlashcardSet
 from flashcards.models import Comment
 from flashcards.models import Collection
 
-
 def index(request):
     context = {
     "title": "Django example",
     }
     return render(request, "index.html", context)
+
+def list_users(request):
+    udata = list(User.objects.values('id', 'username', 'admin'))
+    ujson_data = json.dumps(udata)
+    return render(request, 'list_users.html', {'listuserdata_json': ujson_data})
+    #uqueryset = User.objects.all().values('id', 'username', 'admin')
+    #udata = list(uqueryset)
+    #return JsonResponse(udata, safe=False)
+
 
 #def list_users(request):
     #users = User.objects.all()
