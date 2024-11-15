ba2f41890c9cda007d01f555cc46263b4e2e9bf7
WeebBot#2 <124053467+MiaNSlater@users.noreply.github.com>
2024-11-15 11:40:37 +0000

    Altered FlashcardSet model

--- flashcards/models.py
+++ flashcards/models.py
@@ -34,10 +34,10 @@ class Comment(models.Model):
 
 class FlashcardSet(models.Model):
       name = models.CharField(max_length=100)
-      cards = models.ForeignKey(Flashcard, default=None, on_delete=models.CASCADE,related_name="sets")
+      cards = models.ForeignKey(Flashcard, default=None, null=True, blank=True, on_delete=models.CASCADE,related_name="sets")
       created_at = models.DateTimeField(auto_now_add=True)
       updated_at = models.DateTimeField(auto_now=True)
-      comments = models.ForeignKey(Comment, default=None, on_delete=models.CASCADE)
+      comments = models.ForeignKey(Comment, default=None, null=True, blank=True on_delete=models.CASCADE)
       author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
 class Collection(models.Model):
