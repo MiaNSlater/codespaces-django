03d52509252b58cf9ee87692b0084a4312f5b7fa
WeebBot#2 <124053467+MiaNSlater@users.noreply.github.com>
2024-11-19 12:35:15 +0000

    Edited page, added debugging statements

--- flashcards/core/views.py
+++ flashcards/core/views.py
@@ -33,12 +33,8 @@ def list_collections(request):
     collection_sets = None
     if request.method == 'POST':
         user_id = request.POST.get('user_id')
-        print(f"User ID: {user_id}")
-        try:
-            collection_sets = Collection.objects.filter(author__id=user_id)
-            print(f"Collection User ID: {collection_sets}")
-        except Collection.DoesNotExist:
-            collection_sets = None
+        collectionsets = Collection.objects.filter(author__id=user_id)
+        print(f"Requested Collection Set: {collectionsets}")
     return render(request, 'list_collections.html', {'collection_sets': collection_sets})
 
 def search_id(request):
