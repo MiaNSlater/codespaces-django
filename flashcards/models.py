3fac36cb33e671b21c50cd67c13bfc33e255d7bc
WeebBot#2 <124053467+MiaNSlater@users.noreply.github.com>
2024-11-19 12:17:47 +0000

    Edited collection model and made a migration

--- flashcards/models.py
+++ flashcards/models.py
@@ -46,5 +46,5 @@ class Comment(models.Model):
 
 class Collection(models.Model):
       comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='collection')
-      flashcardset = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name='collection')
+      flashcardset = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, default=None, null=True, blank=True, related_name='collection')
       author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='collection')
